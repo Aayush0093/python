ffcbdd6ae94186350cd045aaae08ab93bdcd651b
Aayush0093 <115064348+Aayush0093@users.noreply.github.com>
2024-05-12 19:37:40 +0530

    Add files via upload

---
+++ program8GUi/GUI.py
@@ -0,0 +1,26 @@
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+# for i in picture:
+#     for j in i:
+#         if(j == 0):
+#             print(' ',end='')
+#         else:
+#             print('*',end='')
+#     print()
+
+for i in range(0,6):
+    for j in range(0,7):
+        if(picture[i][j] == 0):
+            print(' ',end='')
+        else:
+            print('*',end='')
+    print()
